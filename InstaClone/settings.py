957b0f75d6c3ffa394d886e7ccadebb2671e3422
SaudZubedi <SaudZubedi26@gmail.com>
2021-10-01 10:09:11 -0500

    Added HSTS & HTTPS Settings

--- InstaClone/settings.py
+++ InstaClone/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-from .secret import KEY
+from secret import KEY
 SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
